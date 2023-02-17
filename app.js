c8f45865d7c32e347a3b3d02fe991b786a39a3d3
Rely <shark90323@gmail.com>
2023-02-17 18:00:47 +0800

    4.建立好註冊表單和CSS樣式

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const express = require('express');
 
 const app = express();
 
+const baseRoute = require('./routes/base.route');
+const userRoute = require('./routes/user.route');
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 
-const baseRoute = require('./routes/base.route');
-
 app.use(baseRoute);
+app.use(userRoute);
 
 app.listen(3000);
---
+++ controllers/user.controller.js
@@ -0,0 +1,7 @@
+function signup(req, res) {
+  res.render("users/signup");
+}
+
+module.exports = {
+  signup: signup
+}
